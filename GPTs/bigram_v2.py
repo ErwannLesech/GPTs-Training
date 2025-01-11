87e2a059d2a1855e71e763b2e404f149336a788d
erwann.lesech <erwann.lesech@epita.fr>
2025-01-11 20:27:43 +0100

    reorfanize + init house price prediction training

--- GPTs/bigram_v2.py
+++ GPTs/bigram_v2.py
@@ -5,13 +5,13 @@ from torch.nn import functional as F
 # hyperparameters
 batch_size = 64 # how many independent sequences will we process in parallel?
 block_size = 256 # what is the maximum context length for predictions?
-max_iters = 5000
+max_iters = 1000 # 5000
 eval_interval = 500
 learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 64 # 384
-n_head = 6
+n_embd = 6 # 384
+n_head = 4 # 6
 n_layer = 3 #6
 dropout = 0.2
 # ------------
